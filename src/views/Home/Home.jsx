3c24ad9b947366d82d961082aeac7485abfd9580
kashishInamdar <kashishinamdar2004@gmail.com>
2023-11-22 02:58:36 +0530

    add languate to local storage to change the language

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,40 @@
 import "./Home.css"
+import I18n from "../../utills/i18n.jsx"
+import { useState } from "react"
 
 function Home(){
+
+     const usersCount = 15 ;
+
+
+    
     return(
         <>
-            <h1>Home</h1>
+        <h1>{ I18n("welcomeMessage")}</h1>
+
+
+        <p>{I18n("normalMessage")}</p>
+
+
+        <h3>{I18n("greetingMessage")}</h3>
+
+        <p>{I18n("endMassage")}</p>
+
+          <p>
+            {I18n("useStatMassage")}
+        </p> 
+
+        <select
+        defaultValue={localStorage.getItem("lang")}
+        onChange={(e)=>{
+            localStorage.setItem("lang", e.target.value)
+            window.location.reload();
+        }}>
+            <option value="mr">Marathi</option>
+            <option value="hi">Hindi</option>
+            <option value="en">English</option>
+        </select>
+            
         </>
     )
 }
