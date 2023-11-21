5d521d21c23a20818887d946f4d7e223e5feccc8
kashishInamdar <kashishinamdar2004@gmail.com>
2023-11-22 00:44:23 +0530

    initial comit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './views/Home/Home.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Home />
+)
