5c2ae970e96ad61f06c6c961d1df16d39351ed85
kashishInamdar <kashishinamdar2004@gmail.com>
2023-11-22 14:35:59 +0530

    add Css

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,40 +2,44 @@ import "./Home.css"
 import I18n from "../../utills/i18n.jsx"
 import { useState } from "react"
 
-function Home(){
+function Home() {
 
-     const usersCount = 20 ;
+    const usersCount = 20;
 
-
-    
-    return(
+    return (
         <>
-        <h1>{ I18n("welcomeMessage")}</h1>
+           <h1 className="title">INTERNATIONALIZATION</h1>
+            <div className="card">
+                <h1 >{I18n("welcomeMessage")}</h1>
 
 
-        <p>{I18n("normalMessage")}</p>
+                <p className="font6">{I18n("normalMessage")}</p>
 
 
-        <h3>{I18n("greetingMessage")}</h3>
+                <p className="font6" >{I18n("greetingMessage")}</p>
 
-        <p>{I18n("endMassage")}</p>
+                <p className="font6" >{I18n("endMassage")}</p>
 
-          <p>
-            {I18n("useStatMassage" , "<studentcount>" , usersCount)}
-        </p> 
+                <p className="font6">
+                    {I18n("useStatMassage", "<studentcount>", usersCount)}
+                </p>
+                <p className="change-lang">{I18n("language")} </p>
 
-        <select
-        defaultValue={localStorage.getItem("lang")}
-        onChange={(e)=>{
-            localStorage.setItem("lang", e.target.value)
-            window.location.reload();
-        }}>
-            <option value="mr">Marathi</option>
-            <option value="hi">Hindi</option>
-            <option value="en">English</option>
-        </select>
-            
+                <select
+                    className="select-box"
+                    defaultValue={localStorage.getItem("lang")}
+                    onChange={(e) => {
+                        localStorage.setItem("lang", e.target.value)
+                        window.location.reload();
+                    }}>
+                    <option value="mr">मराठी </option>
+                    <option value="hi">हिंदी </option>
+                    <option value="en">English</option>
+                </select>
+
+            </div>
+
         </>
     )
 }
-export default Home
+export default Home  
